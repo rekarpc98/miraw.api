d3b0c103a61564c9f86b13f68f5993537ce8320d
rekardev1 <rekardev1@gmail.com>
2023-10-07 14:24:07 +0300

    Broker -> Add StorageBroker.Zone

---
+++ Miraw.Api.Core/Brokers/Storages/StorageBroker.Zone.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Miraw.Api.Core.Models.Zones;
+
+namespace Miraw.Api.Core.Brokers.Storages;
+
+public partial class StorageBroker
+{
+    DbSet<Zone> Zones { get; set; }
+    
+    public async ValueTask<Zone> InsertZoneAsync(Zone zone) => await InsertAsync(zone);
+
+    public IQueryable<Zone> SelectAllZones() => SelectAll<Zone>();
+
+    public async ValueTask<Zone?> SelectZoneByIdAsync(Guid zoneId) => await SelectAsync<Zone>(zoneId);
+
+    public async ValueTask<Zone> UpdateZoneAsync(Zone zone) => await UpdateAsync(zone);
+
+    public async ValueTask<Zone> DeleteZoneAsync(Zone zone) => await DeleteAsync(zone);
+}
---
+++ Miraw.Api.Core/Models/Zones/Zone.cs
@@ -0,0 +1,13 @@
+﻿using Miraw.Api.Core.Models.Commons;
+using Miraw.Api.Core.Models.Regions;
+using NetTopologySuite.Geometries;
+
+namespace Miraw.Api.Core.Models.Zones;
+
+public class Zone : Auditable
+{
+	public Guid Id { get; set; }
+	public Guid RegionId { get; set; }
+	public Region? Region { get; set; }
+	public Geometry Boundary { get; set; } = null!;
+}
