597d9101d7c6488eb968bbea3fc288d0c3cd94a0
rekardev1 <rekardev1@gmail.com>
2023-10-07 17:57:56 +0300

    Refactoring

--- Miraw.Api.Core/Brokers/Storages/StorageBroker.Region.cs
+++ Miraw.Api.Core/Brokers/Storages/StorageBroker.Region.cs
@@ -1,20 +1,19 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Miraw.Api.Core.Models.Regions;
 
 namespace Miraw.Api.Core.Brokers.Storages;
 
 public partial class StorageBroker
 {
-    DbSet<Region> Regions { get; set; }
-    
-    public async ValueTask<Region> InsertRegionAsync(Region region) => await InsertAsync(region);
+	DbSet<Region> Regions { get; set; }
 
-    public IQueryable<Region> SelectAllRegions() => SelectAll<Region>();
+	public async ValueTask<Region> InsertRegionAsync(Region region) => await InsertAsync(region);
 
-    public async ValueTask<Region?> SelectRegionByIdAsync(Guid regionId) => await SelectAsync<Region>(regionId);
+	public IQueryable<Region> SelectAllRegions() => SelectAll<Region>();
 
-    public async ValueTask<Region> UpdateRegionAsync(Region region) => await UpdateAsync(region);
+	public async ValueTask<Region?> SelectRegionByIdAsync(Guid regionId) => await SelectAsync<Region>(regionId);
 
-    public async ValueTask<Region> DeleteRegionAsync(Region region) => await DeleteAsync(region);
+	public async ValueTask<Region> UpdateRegionAsync(Region region) => await UpdateAsync(region);
+
+	public async ValueTask<Region> DeleteRegionAsync(Region region) => await DeleteAsync(region);
 }
