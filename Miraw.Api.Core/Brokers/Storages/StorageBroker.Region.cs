7ca8651e297cd461d9f616fa0880decdeadb47af
rekardev1 <rekardev1@gmail.com>
2023-10-07 14:19:26 +0300

    Broker -> Add StorageBroker.Region

---
+++ Miraw.Api.Core/Brokers/Storages/StorageBroker.Region.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Miraw.Api.Core.Models.Regions;
+
+namespace Miraw.Api.Core.Brokers.Storages;
+
+public partial class StorageBroker
+{
+    DbSet<Region> Regions { get; set; }
+    
+    public async ValueTask<Region> InsertRegionAsync(Region region) => await InsertAsync(region);
+
+    public IQueryable<Region> SelectAllRegions() => SelectAll<Region>();
+
+    public async ValueTask<Region?> SelectRegionByIdAsync(Guid regionId) => await SelectAsync<Region>(regionId);
+
+    public async ValueTask<Region> UpdateRegionAsync(Region region) => await UpdateAsync(region);
+
+    public async ValueTask<Region> DeleteRegionAsync(Region region) => await DeleteAsync(region);
+}
--- Miraw.Api.Core/Models/Regions/Region.cs
+++ Miraw.Api.Core/Models/Regions/Region.cs
@@ -1,8 +1,11 @@
 ﻿using Miraw.Api.Core.Models.Commons;
+using NetTopologySuite.Geometries;
 
 namespace Miraw.Api.Core.Models.Regions;
 
 public class Region : Auditable
 {
-	
+	public Guid Id { get; set; }
+	public string Name { get; set; } = null!;
+	public Geometry Boundary { get; set; } = null!;
 }
