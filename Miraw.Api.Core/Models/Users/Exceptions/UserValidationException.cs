c4f6175d347b921b48745e00cea5ac916f0df3ba
rekardev1 <rekardev1@gmail.com>
2023-10-08 21:26:33 +0300

    Add UserValidationException() Constructor

--- Miraw.Api.Core/Models/Users/Exceptions/UserValidationException.cs
+++ Miraw.Api.Core/Models/Users/Exceptions/UserValidationException.cs
@@ -4,6 +4,10 @@ namespace Miraw.Api.Core.Models.Users.Exceptions;
 
 public class UserValidationException : Xeption
 {
+	public UserValidationException() : base(message: "Invalid input, contact support.")
+	{
+	}
+
 	public UserValidationException(Exception innerException) : base(message: "Invalid input, contact support.",
 		innerException)
 	{
