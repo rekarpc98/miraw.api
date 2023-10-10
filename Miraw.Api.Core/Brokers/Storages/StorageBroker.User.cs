e5dd97b645d89ba43c94e5d217452f9339d8db72
rekardev1 <rekardev1@gmail.com>
2023-10-10 05:56:09 +0300

    Broker: Add SelectUserByNameAsync to StorageBroker.User

--- Miraw.Api.Core/Brokers/Storages/StorageBroker.User.cs
+++ Miraw.Api.Core/Brokers/Storages/StorageBroker.User.cs
@@ -1,20 +1,25 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Miraw.Api.Core.Models.Users;
 
 namespace Miraw.Api.Core.Brokers.Storages;
 
 public partial class StorageBroker
 {
-    DbSet<User> Users { get; set; }
-    
-    public async ValueTask<User> InsertUserAsync(User user) => await InsertAsync(user);
+	DbSet<User> Users { get; set; }
 
-    public IQueryable<User> SelectAllUsers() => SelectAll<User>();
+	public async ValueTask<User> InsertUserAsync(User user) => await InsertAsync(user);
 
-    public async ValueTask<User?> SelectUserByIdAsync(Guid userId) => await SelectAsync<User>(userId);
+	public IQueryable<User> SelectAllUsers() => SelectAll<User>();
 
-    public async ValueTask<User> UpdateUserAsync(User user) => await UpdateAsync(user);
+	public async ValueTask<User?> SelectUserByIdAsync(Guid userId) => await SelectAsync<User>(userId);
 
-    public async ValueTask<User> DeleteUserAsync(User user) => await DeleteAsync(user);
+	public async ValueTask<User?> SelectUserByNameAsync(string userName)
+	{
+		userName = userName.Trim();
+		return await Users.FirstOrDefaultAsync(user => user.Name == userName);
+	}
+
+	public async ValueTask<User> UpdateUserAsync(User user) => await UpdateAsync(user);
+
+	public async ValueTask<User> DeleteUserAsync(User user) => await DeleteAsync(user);
 }
