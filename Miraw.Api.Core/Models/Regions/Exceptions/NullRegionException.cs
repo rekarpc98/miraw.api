8a87aff462d0ab482b92fed8ba75b43701cbaa00
rekardev1 <rekardev1@gmail.com>
2023-10-13 22:05:54 +0300

    Add Region exception models

---
+++ Miraw.Api.Core/Models/Regions/Exceptions/NullRegionException.cs
@@ -0,0 +1,12 @@
+﻿
+using Xeptions;
+
+namespace Miraw.Api.Core.Models.Regions.Exceptions;
+
+public class NullRegionException : Xeption
+{
+	public NullRegionException() : base("Region is null.")
+	{
+		
+	}
+}
---
+++ Miraw.Api.Core/Models/Regions/Exceptions/RegionValidationException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace Miraw.Api.Core.Models.Regions.Exceptions;
+
+public class RegionValidationException : Xeption
+{
+	public RegionValidationException(Exception innerException)
+		: base(message: "Invalid input, contact support.", innerException)
+	{
+	}
+}
