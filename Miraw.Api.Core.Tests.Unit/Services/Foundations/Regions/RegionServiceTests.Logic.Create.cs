9ab63b4e3a202e620b430d98f4902ec4c0232e59
rekardev1 <rekardev1@gmail.com>
2023-10-13 21:33:13 +0300

    ShouldCreateRegionAsync -> FAIL

---
+++ Miraw.Api.Core.Tests.Unit/Services/Foundations/Regions/RegionServiceTests.Logic.Create.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Miraw.Api.Core.Models.Regions;
+using Moq;
+
+namespace Miraw.Api.Core.Tests.Unit.Services.Foundations.Regions;
+
+public partial class RegionServiceTests
+{
+	[Fact]
+	public async Task ShouldCreateRegionAsync()
+	{
+		// given
+		Region randomRegion = CreateRandomRegion();
+		Region inputRegion = randomRegion.DeepClone();
+		Region storageRegion = randomRegion.DeepClone();
+		Region expectedRegion = storageRegion.DeepClone();
+
+		_storageBrokerMock.Setup(broker =>
+			broker.InsertRegionAsync(inputRegion))
+				.ReturnsAsync(storageRegion);
+
+		// when
+		Region actualRegion =
+			await _regionService.CreateRegionAsync(inputRegion);
+
+		// then
+		actualRegion.Should().BeEquivalentTo(expectedRegion);
+
+		_storageBrokerMock.Verify(broker =>
+			broker.InsertRegionAsync(inputRegion),
+				Times.Once);
+
+		_storageBrokerMock.VerifyNoOtherCalls();
+	}
+}
---
+++ Miraw.Api.Core.Tests.Unit/Services/Foundations/Regions/RegionServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Miraw.Api.Core.Brokers.Storages;
+using Miraw.Api.Core.Models.Regions;
+using Miraw.Api.Core.Services.Foundations.Regions;
+using Moq;
+using NetTopologySuite.Geometries;
+using Tynamix.ObjectFiller;
+
+namespace Miraw.Api.Core.Tests.Unit.Services.Foundations.Regions;
+
+public partial class RegionServiceTests
+{
+	readonly Mock<IStorageBroker> _storageBrokerMock;
+	readonly IRegionService _regionService;
+
+	public RegionServiceTests()
+	{
+		_storageBrokerMock = new Mock<IStorageBroker>();
+
+		_regionService = new RegionService(_storageBrokerMock.Object);
+	}
+
+	static Region CreateRandomRegion()
+	{
+		Filler<Region> filler = CreateUserFiller(DateTimeOffset.UtcNow);
+		
+		return filler.Create();
+	}
+	
+	static Filler<Region> CreateUserFiller(DateTimeOffset date)
+	{
+		var filler = new Filler<Region>();
+		Guid userId = Guid.NewGuid();
+		Guid regionId = Guid.NewGuid();
+
+		filler.Setup()
+			.OnProperty(x => x.Id)
+			.Use(regionId)
+			.OnProperty(x => x.CreatedDate)
+			.Use(date)
+			.OnProperty(x => x.UpdatedDate)
+			.Use(date)
+			.OnProperty(x => x.DeletedDate)
+			.IgnoreIt()
+			.OnProperty(x => x.DeletedBy)
+			.IgnoreIt()
+			.OnProperty(x => x.CreatedBy)
+			.Use(userId)
+			.OnProperty(x => x.UpdatedBy)
+			.Use(userId)
+			.OnProperty(x => x.Name)
+			.Use(new MnemonicString().GetValue())
+			.OnProperty(x => x.Boundary)
+			.Use(new Polygon(new LinearRing(new[]
+			{
+				new Coordinate(0, 0),
+				new Coordinate(0, 1),
+				new Coordinate(1, 1),
+				new Coordinate(1, 0),
+				new Coordinate(0, 0)
+			})));
+
+		return filler;
+	}
+}
