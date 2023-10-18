6109a6c73309824839c26414973bb9c85b885495
rekardev1 <rekardev1@gmail.com>
2023-10-18 11:30:00 +0300

    ShouldThrowValidationExceptionOnRetrieveWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ Miraw.Api.Core.Tests.Unit/Services/Foundations/Regions/RegionServiceTests.Exceptions.RetrievRegionById.cs
@@ -0,0 +1,33 @@
+﻿using Miraw.Api.Core.Models.Regions;
+using Miraw.Api.Core.Models.Regions.Exceptions;
+using Moq;
+
+namespace Miraw.Api.Core.Tests.Unit.Services.Foundations.Regions;
+
+public partial class RegionServiceTests
+{
+	[Fact]
+	public void ShouldThrowValidationExceptionOnRetrieveWhenIdIsInvalidAndLogItAsync()
+	{
+		// given
+		var invalidRegionId = Guid.NewGuid();
+
+		var notFoundRegionException =
+			new NotFoundRegionException(invalidRegionId);
+        
+		var expectedRegionValidationException =
+			new RegionValidationException(notFoundRegionException);
+
+		// when
+		ValueTask<Region> retrieveRegionTask =
+			_regionService.GetRegionAsync(invalidRegionId);
+
+		// then
+		Assert.ThrowsAsync<RegionValidationException>(() =>
+			retrieveRegionTask.AsTask());
+
+		_loggingBrokerMock.Verify(x =>
+				x.LogError(It.Is(SameExceptionAs(expectedRegionValidationException))),
+			Times.Once());
+	}
+}
