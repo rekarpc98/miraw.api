20cc506a1734d02cb3f8253a934211c63d1cf3a5
rekardev1 <rekardev1@gmail.com>
2023-10-24 06:18:18 +0300

    ShouldThrowZoneValidationExceptionOnCreateWhenIdIsInvalidAndLogIt -> FAIL

--- Miraw.Api.Core.Tests.Unit/Services/Foundations/Zones/ZoneServiceTests.Validation.Create.cs
+++ Miraw.Api.Core.Tests.Unit/Services/Foundations/Zones/ZoneServiceTests.Validation.Create.cs
@@ -12,23 +12,56 @@ public partial class ZoneServiceTests
 		// given
 		Zone? nullZone = null;
 		var nullZoneException = new NullZoneException();
-		
+
 		var zoneValidationException = new ZoneValidationException(nullZoneException);
-		
+
 		// when
 		ValueTask<Zone> createZoneTask = zoneService.CreateZoneAsync(nullZone!);
-		
+
 		// then
 		Assert.ThrowsAsync<ZoneValidationException>(() => createZoneTask.AsTask());
-		
+
 		loggingBrokerMock.Verify(broker =>
-			broker.LogError(It.Is(SameExceptionAs(zoneValidationException))),
+				broker.LogError(It.Is(SameExceptionAs(zoneValidationException))),
 			Times.Once);
-		
+
 		storageBrokerMock.Verify(broker =>
-			broker.InsertZoneAsync(It.IsAny<Zone>()),
+				broker.InsertZoneAsync(It.IsAny<Zone>()),
 			Times.Never);
-		
+
+		storageBrokerMock.VerifyNoOtherCalls();
+	}
+
+	[Fact]
+	public void ShouldThrowZoneValidationExceptionOnCreateWhenIdIsInvalidAndLogIt()
+	{
+		// given
+		Guid invalidId = Guid.Empty;
+		Zone randomZone = CreateRandomZone(invalidId);
+		Zone invalidZone = randomZone;
+
+		var invalidZoneException = new InvalidZoneException();
+
+		invalidZoneException.AddData(
+			key: nameof(Zone.Id),
+			values: "Invalid zone id");
+
+		var zoneValidationException = new ZoneValidationException(invalidZoneException);
+
+		// when
+		ValueTask<Zone> createZoneTask = zoneService.CreateZoneAsync(invalidZone);
+
+		// then
+		Assert.ThrowsAsync<ZoneValidationException>(() => createZoneTask.AsTask());
+
+		loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(zoneValidationException))),
+			Times.Once);
+
+		storageBrokerMock.Verify(broker =>
+				broker.InsertZoneAsync(It.IsAny<Zone>()),
+			Times.Never);
+
 		storageBrokerMock.VerifyNoOtherCalls();
 	}
 }
