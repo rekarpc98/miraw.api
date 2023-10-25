5705c58838dfaace8c3789476c6d6aafca8f819d
rekardev1 <rekardev1@gmail.com>
2023-10-25 18:38:11 +0300

    ShouldThrowZoneValidationExceptionOnRetrieveByIdWhenInputIdIsInvalidAndLogItAsync -> FAIL

--- Miraw.Api.Core/Models/Zones/Exceptions/InvalidZoneException.cs
+++ Miraw.Api.Core/Models/Zones/Exceptions/InvalidZoneException.cs
@@ -7,4 +7,9 @@ public class InvalidZoneException : Xeption
 	public InvalidZoneException() : base(message: "Invalid zone. Please fix the errors and try again.")
 	{
 	}
+
+	public InvalidZoneException(string parameterName, object parameterValue) : base(
+		message: $"Invalid zone, parameter name: {parameterName}, parameter value: {parameterValue}.")
+	{
+	}
 }
