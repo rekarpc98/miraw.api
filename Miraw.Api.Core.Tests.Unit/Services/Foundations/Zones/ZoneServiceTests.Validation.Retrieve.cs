b344542e453824d9b898170c1ff7b12fbd55e8b2
rekardev1 <rekardev1@gmail.com>
2023-10-25 18:47:53 +0300

    ShouldThrowValidationExceptionOnRetrieveByIdWhenStorageZoneIsNullAndLogItAsync -> FAIL

--- Miraw.Api.Core.Tests.Unit/Services/Foundations/Zones/ZoneServiceTests.Validation.Retrieve.cs
+++ Miraw.Api.Core.Tests.Unit/Services/Foundations/Zones/ZoneServiceTests.Validation.Retrieve.cs
@@ -1,4 +1,5 @@
-﻿using Miraw.Api.Core.Models.Zones;
+﻿using System.Diagnostics.Eventing.Reader;
+using Miraw.Api.Core.Models.Zones;
 using Miraw.Api.Core.Models.Zones.Exceptions;
 using Moq;
 
@@ -17,8 +18,8 @@ public partial class ZoneServiceTests
 		var expectedZoneValidationException = new ZoneValidationException(invalidZoneException);
 
 		storageBrokerMock.Setup(broker =>
-			broker.SelectZoneByIdAsync(invalidId))
-				.ThrowsAsync(invalidZoneException);
+				broker.SelectZoneByIdAsync(invalidId))
+			.ThrowsAsync(invalidZoneException);
 
 		// when
 		ValueTask<Zone> retrieveZoneByIdTask = zoneService.RetrieveZoneByIdAsync(inputId);
@@ -27,13 +28,45 @@ public partial class ZoneServiceTests
 		await Assert.ThrowsAsync<ZoneValidationException>(() => retrieveZoneByIdTask.AsTask());
 
 		storageBrokerMock.Verify(broker =>
-			broker.SelectZoneByIdAsync(invalidId),
-				Times.Never);
-		
+				broker.SelectZoneByIdAsync(invalidId),
+			Times.Never);
+
 		loggingBrokerMock.Verify(broker =>
-			broker.LogError(It.Is(SameExceptionAs(expectedZoneValidationException))),
-				Times.Once);
-		
+				broker.LogError(It.Is(SameExceptionAs(expectedZoneValidationException))),
+			Times.Once);
+
 		storageBrokerMock.VerifyNoOtherCalls();
+	}
+
+	[Fact]
+	public async Task ShouldThrowValidationExceptionOnRetrieveByIdWhenStorageZoneIsNullAndLogItAsync()
+	{
+		// given
+
+		var randomGuid = Guid.NewGuid();
+		Zone? nullZone = null;
+		Zone? storageZone = nullZone;
+		var notFoundZoneException = new NotFoundZoneException(randomGuid);
+		var expectedZoneValidationException = new ZoneValidationException(notFoundZoneException);
+
+		storageBrokerMock.Setup(x =>
+				x.SelectZoneByIdAsync(randomGuid))
+			.ReturnsAsync(storageZone);
+
+		// when
+
+		ValueTask<Zone> retrieveZoneByIdTask = zoneService.RetrieveZoneByIdAsync(randomGuid);
+
+		// then
+
+		await Assert.ThrowsAsync<ZoneValidationException>(() => retrieveZoneByIdTask.AsTask());
+
+		storageBrokerMock.Verify(x => x.SelectZoneByIdAsync(randomGuid), Times.Once);
+
+		storageBrokerMock.VerifyNoOtherCalls();
+
+		loggingBrokerMock.Verify(x =>
+				x.LogError(It.Is(SameExceptionAs(expectedZoneValidationException))),
+			Times.Once);
 	}
 }
---
+++ Miraw.Api.Core/Models/Zones/Exceptions/NotFoundZoneException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace Miraw.Api.Core.Models.Zones.Exceptions;
+
+public class NotFoundZoneException : Xeption
+{
+	public NotFoundZoneException(Guid zoneId)
+		: base(message: $"Couldn't find zone with id: {zoneId}.")
+	{
+	}
+}
