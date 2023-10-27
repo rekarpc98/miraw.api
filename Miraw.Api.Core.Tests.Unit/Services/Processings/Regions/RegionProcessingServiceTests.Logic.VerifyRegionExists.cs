db5fccac1410905b81dfff225cbae265ac867c3e
rekardev1 <rekardev1@gmail.com>
2023-10-27 13:00:30 +0300

    ShouldReturnFalseIfRegionNotExists -> FAIL

--- Miraw.Api.Core.Tests.Unit/Services/Processings/Regions/RegionProcessingServiceTests.Logic.VerifyRegionExists.cs
+++ Miraw.Api.Core.Tests.Unit/Services/Processings/Regions/RegionProcessingServiceTests.Logic.VerifyRegionExists.cs
@@ -8,7 +8,7 @@ namespace Miraw.Api.Core.Tests.Unit.Services.Processings.Regions;
 public partial class RegionProcessingServiceTests
 {
 	[Fact]
-	public async Task ShouldVerifyRegionIfExists()
+	public async Task ShouldReturnTrueIfRegionExists()
 	{
 		// given
 		var randomRegionId = Guid.NewGuid();
@@ -16,6 +16,36 @@ public partial class RegionProcessingServiceTests
 		Region randomRegion = CreateRandomRegion(randomRegionId);
 		Region storageRegion = randomRegion;
 		const bool expected = true;
+		
+		regionServiceMock.Setup(x => x.RetrieveRegionAsync(inputRegionId))
+			.ReturnsAsync(storageRegion);
+		
+		// when
+		bool actual = await regionProcessingService.VerifyRegionExistsAsync(inputRegionId);
+		
+		// then
+		
+		actual.Should().Be(expected);
+		
+		regionServiceMock.Verify(x => 
+				x.RetrieveRegionAsync(inputRegionId),
+			Times.Once);
+		
+		loggingBrokerMock.Verify(x => x.LogError(It.IsAny<Exception>()), Times.Never);
+		
+		regionServiceMock.VerifyNoOtherCalls();
+		loggingBrokerMock.VerifyNoOtherCalls();
+	}
+	
+	[Fact]
+	public async Task ShouldReturnFalseIfRegionNotExists()
+	{
+		// given
+		var randomRegionId = Guid.NewGuid();
+		Guid inputRegionId = randomRegionId;
+		Region nullRegion = null!;
+		Region storageRegion = nullRegion;
+		const bool expected = false;
 		
 		regionServiceMock.Setup(x => x.RetrieveRegionAsync(inputRegionId))
 			.ReturnsAsync(storageRegion);
