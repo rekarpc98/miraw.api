00bfd1e757425df77e94b66dc3e360a8cd2184f6
rekardev1 <rekardev1@gmail.com>
2023-10-31 19:54:47 +0300

    Processing Services: Add PasswordProcessingService

---
+++ Miraw.Api.Core/Services/Processings/Passwords/IPasswordProcessingService.cs
@@ -0,0 +1,8 @@
+﻿using Miraw.Api.Core.Models.Passwords;
+
+namespace Miraw.Api.Core.Services.Processings.Passwords;
+
+public interface IPasswordProcessingService
+{
+	ValueTask<Password> CreatePasswordAsync(Password password);
+}
---
+++ Miraw.Api.Core/Services/Processings/Passwords/PasswordProcessingService.cs
@@ -0,0 +1,19 @@
+﻿using Miraw.Api.Core.Models.Passwords;
+using Miraw.Api.Core.Services.Foundations.Passwords;
+
+namespace Miraw.Api.Core.Services.Processings.Passwords;
+
+public class PasswordProcessingService : IPasswordProcessingService
+{
+	private readonly IPasswordService passwordService;
+
+	public PasswordProcessingService(IPasswordService passwordService)
+	{
+		this.passwordService = passwordService;
+	}
+	
+	public async ValueTask<Password> CreatePasswordAsync(Password password)
+	{
+		throw new NotImplementedException();
+	}
+}
