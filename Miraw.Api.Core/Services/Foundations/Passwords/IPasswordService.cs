7a7b119ce04e63c8718d5a200e22c94eb414d618
rekardev1 <rekardev1@gmail.com>
2023-10-31 19:22:44 +0300

    Foundations: Add PasswordService

---
+++ Miraw.Api.Core/Services/Foundations/Passwords/IPasswordService.cs
@@ -0,0 +1,10 @@
+﻿using Miraw.Api.Core.Models.Passwords;
+
+namespace Miraw.Api.Core.Services.Foundations.Passwords;
+
+public interface IPasswordService
+{
+	ValueTask<Password> CreatePasswordAsync(Password password);
+	ValueTask<Password> UpdatePasswordAsync(Password password);
+	ValueTask<Password> RetrievePasswordByUserIdAsync(Guid userId);
+}
---
+++ Miraw.Api.Core/Services/Foundations/Passwords/PasswordService.cs
@@ -0,0 +1,21 @@
+﻿using Miraw.Api.Core.Models.Passwords;
+
+namespace Miraw.Api.Core.Services.Foundations.Passwords;
+
+public class PasswordService : IPasswordService
+{
+	public async ValueTask<Password> CreatePasswordAsync(Password password)
+	{
+		throw new NotImplementedException();
+	}
+
+	public async ValueTask<Password> UpdatePasswordAsync(Password password)
+	{
+		throw new NotImplementedException();
+	}
+
+	public async ValueTask<Password> RetrievePasswordByUserIdAsync(Guid userId)
+	{
+		throw new NotImplementedException();
+	}
+}
