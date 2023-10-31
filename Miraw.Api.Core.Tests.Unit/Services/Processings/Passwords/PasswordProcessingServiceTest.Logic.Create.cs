e5eedcbdeacbe25096af19000995bcc0e75aa053
rekardev1 <rekardev1@gmail.com>
2023-10-31 19:59:44 +0300

    ShouldCreatePasswordAsync -> FAIL

---
+++ Miraw.Api.Core.Tests.Unit/Services/Processings/Passwords/PasswordProcessingServiceTest.Logic.Create.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Miraw.Api.Core.Models.Passwords;
+using Moq;
+
+namespace Miraw.Api.Core.Tests.Unit.Services.Processings.Passwords;
+
+public partial class PasswordProcessingServiceTest
+{
+	[Fact]
+	public async Task ShouldCreatePasswordAsync()
+	{
+		// given
+		Password randomPassword = CreateRandomPassword();
+		Password inputPassword = randomPassword;
+		Password storagePassword = randomPassword;
+		Password expectedPassword = storagePassword;
+
+		passwordServiceMock.Setup(broker =>
+				broker.CreatePasswordAsync(inputPassword))
+			.ReturnsAsync(storagePassword);
+
+		// when
+		Password actualPassword = await passwordProcessingService.CreatePasswordAsync(inputPassword);
+
+		// then
+		actualPassword.Should().BeEquivalentTo(expectedPassword);
+
+		passwordServiceMock.Verify(broker =>
+				broker.CreatePasswordAsync(inputPassword),
+			Times.Once);
+
+		passwordServiceMock.VerifyNoOtherCalls();
+	}
+}
---
+++ Miraw.Api.Core.Tests.Unit/Services/Processings/Passwords/PasswordProcessingServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Miraw.Api.Core.Brokers.Storages;
+using Miraw.Api.Core.Models.Passwords;
+using Miraw.Api.Core.Services.Foundations.Passwords;
+using Miraw.Api.Core.Services.Processings.Passwords;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace Miraw.Api.Core.Tests.Unit.Services.Processings.Passwords;
+
+public partial class PasswordProcessingServiceTest
+{
+	private readonly IPasswordProcessingService passwordProcessingService;
+	private readonly Mock<IPasswordService> passwordServiceMock = new();
+	
+	public PasswordProcessingServiceTest()
+	{
+		passwordProcessingService = new PasswordProcessingService(passwordServiceMock.Object);
+	}
+	
+	private Password CreateRandomPassword()
+	{
+		return CreatePasswordFiller().Create();
+	}
+	
+	private static Filler<Password> CreatePasswordFiller()
+	{
+		var filler = new Filler<Password>();
+
+		filler.Setup()
+			.OnType<DateTimeOffset>()
+			.Use(CreateRandomDateTimeOffset())
+			.OnType<DateTimeOffset?>()
+			.IgnoreIt()
+			.OnProperty(x => x.User)
+			.IgnoreIt();
+
+		return filler;
+	}
+
+	private static DateTimeOffset CreateRandomDateTimeOffset()
+	{
+		return DateTimeOffset.UtcNow;
+	}
+}
